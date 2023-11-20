716c81ec2bdcffcbdff0bab7a4df984b75a85bbf
biaaoliveira <bihlives@hotmail.com>
2023-11-20 15:45:49 -0300

    insere a nova versão do projeto integrado com o mongodb

--- mulheres.js
+++ mulheres.js
@@ -1,7 +1,6 @@
 const express = require("express") //aqui estou iniciando o express
 const router = express.Router() //aqui estou configurando a primeira parte da rota
 const cors = require('cors') //aqui estou trazendo o pacote cors, que permite consumir esta API no front-end
-
 const conectaBancoDeDados = require('./bancoDeDados') //aqui estou ligando ao arquivo bancoDeDados
 conectaBancoDeDados() // aqui estou chamando a função que conecta o banco de dados
 
@@ -10,6 +9,7 @@ const Mulher = require('./mulherModel')
 const app = express()//aqui estou iniciando o app
 app.use(express.json())
 app.use(cors())
+
 const porta = 3333//aqui estou criando a porta
 
 //GET
@@ -27,7 +27,7 @@ async function mostraMulheres(request, response) {
 async function criaMulher(request, response) {
     const novaMulher = new Mulher({
         nome: request.body.nome,
-        image: request.body.imagem,
+        imagem: request.body.imagem,
         minibio: request.body.minibio,
         citacao: request.body.citacao
     })
@@ -36,7 +36,6 @@ async function criaMulher(request, response) {
         const mulherCriada = await novaMulher.save()
         response.status(201).json(mulherCriada)
     } catch (erro) {
-
         console.log(erro)
     }
 }
@@ -63,6 +62,7 @@ async function corrigeMulher(request, response) {
         }
 
         const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+
         response.json(mulherAtualizadaNoBancoDeDados)
     } catch (erro) {
         console.log(erro)
@@ -79,13 +79,15 @@ async function deletaMulher(request, response) {
     }
 }
 
+app.use(router.get('/mulheres', mostraMulheres))//configurei rota GET /mulheres
+app.use(router.post('/mulheres', criaMulher))//configurei rota POST /mulheres
+app.use(router.patch('/mulheres/:id', corrigeMulher)) //configurei a rota PATCH / mulheres
+app.use(router.delete('/mulheres/:id', deletaMulher)) // configurei rota DELETE/mulheres
+
+
 //PORTA
 function mostraPorta() {
     console.log("Servidor criado e rodando na porta", porta)
 }
 
-app.use(router.get('/mulheres', mostraMulheres))//configurei rota GET /mulheres
-app.use(router.post('/mulheres', criaMulher))//configurei rota POST /mulheres
-app.use(router.patch('/mulheres/:id', corrigeMulher)) //configurei a rota PATCH / mulheres
 app.listen(porta, mostraPorta) //servidor OUVINDO A PORTA
-app.use(router.delete('/mulheres/:id', deletaMulher)) // configurei rota DELETE/mulheres
